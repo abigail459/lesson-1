728e9e069bd044c9c0c5cd7adae1cb16e6225caf
abigail <lau_rei_yan_abigail@s2023.ssts.edu.sg>
2024-04-08 14:25:32 +0800

    Initial Commit

---
+++ lesson-1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lesson-1
+//
+//  Created by Abigail Lau on 8/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lesson-1/lesson_1App.swift
@@ -0,0 +1,17 @@
+//
+//  lesson_1App.swift
+//  lesson-1
+//
+//  Created by Abigail Lau on 8/4/24.
+//
+
+import SwiftUI
+
+@main
+struct lesson_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
